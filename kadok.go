056ec46135943a8a033c2539670259c8cb87c55f
terag <victor@rouquette.me>
2019-11-20 23:08:36 +0100

    Kadok now replies when his name is called

--- kadok.go
+++ kadok.go
@@ -1,7 +1,110 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// Variables used globally
+var (
+	Token string
+	Kadok Character
+)
+
+var buffer = make([][]byte, 0)
+
+// Character of Kaamelott to retrieve sentences from
+type Character struct {
+	Sentences []string `json:"sentences"`
+}
+
+func init() {
+
+	jsonFile, err := os.Open("characters/kadok.json")
+
+	if err != nil {
+		fmt.Println("Error getting Kadok's sentences")
+	} else {
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(byteValue, &Kadok)
+		fmt.Println("Kadok loaded succesfully!")
+	}
+
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the autenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// If the message is "ping" reply with "Pong!"
+	if strings.ToUpper(m.Content) == "PING" {
+		s.ChannelMessageSend(m.ChannelID, "À Kadoc ! À Kadoc ! Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if strings.ToUpper(m.Content) == "PONG" {
+		s.ChannelMessageSend(m.ChannelID, "À Kadoc ! À Kadoc ! Ping!")
+	}
+
+	if strings.Index(strings.ToUpper(m.Content), "KADOK") > -1 {
+		if len(Kadok.Sentences) < 1 {
+			s.ChannelMessageSend(m.ChannelID, "Mordu, mordu mordu moooooooooooooooordu mordu mordu mordu mordu mordu mordu mordu mordu mordu mordu mordu morduuuuuuuuuuuuuuuuuuuuuuuuuuuuu!!!!")
+			return
+		}
+
+		index := rand.Intn(len(Kadok.Sentences))
+
+		s.ChannelMessageSend(m.ChannelID, Kadok.Sentences[index])
+	}
+
+}
+
+func sayRandom(s *discordgo.Session) {
+
 }
