4a64686d8b6069566024d018d336d36581d58ce0
Terag <git@rouquette.me>
2020-04-17 17:07:41 +0200

    Implements RBAC (#9)
    Add .vscode folder to gitignore
    
    Removes .vscode files
    
    Implements function to retrieve user roles from discord
    
    Add basic roles structure configuration
    
    Add custom UnMarschaller for Permission
    
    Adds permission control to basic commands
    
    Rework Security Implementation Design

---
+++ security/security_test.go
@@ -0,0 +1,27 @@
+package security
+
+import "testing"
+
+func LoadRolesTree(t *testing.T) {
+	rolesTree, err := MakeRolesTreeFromFile("roles.json")
+	if err != nil {
+		t.Errorf("Error loading all roles member, invalid number, error: %v", err)
+	}
+	if len(rolesTree.Buffer) != 11 {
+		t.Errorf("Error loading roles, got: %v want 11", len(rolesTree.Buffer))
+	}
+	if !rolesTree.Roles["Admin - Conseil de Guerre"].IsGranted(CallCharacter) {
+		t.Errorf("Error checking generated permission hierarchy")
+	}
+}
+
+func TestRolesTreeIsGranted(t *testing.T) {
+	rolesTree, err := MakeRolesTreeFromFile("roles.json")
+	if err != nil {
+		t.Errorf("Error loading all roles member, invalid number, error: %v", err)
+	}
+	isGranted := MakeIsGranted(rolesTree, []string{"Admin - Conseil de Guerre"})
+	if !isGranted(CallCharacter) {
+		t.Errorf("Error checking generated permission hierarchy")
+	}
+}
